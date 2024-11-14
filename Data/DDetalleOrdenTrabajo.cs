2be3262d60c89d384284c04eb3a0c4415a3bbb09
xdocimo <34216821+xdocimo@users.noreply.github.com>
2024-11-13 23:45:17 -0300

    ricaso el sapito

--- Data/DDetalleOrdenTrabajo.cs
+++ Data/DDetalleOrdenTrabajo.cs
@@ -2,21 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Xml.Linq;
-using System.Data.SqlClient;
 using System.Configuration;
-
 
 namespace Datos
 {
     public class DDetalleOrdenTrabajo
     {
-
+        // Método para eliminar un detalle de orden de trabajo
         public static void DeleteDetalleOrdenTrabajo(int id)
         {
             try
             {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
 
                 using (SqlConnection connection = new SqlConnection(conexion))
                 {
@@ -25,8 +22,8 @@ namespace Datos
                     command.Parameters.AddWithValue("@id", id);
 
                     connection.Open();
-                    command.ExecuteNonQuery(); 
-                } 
+                    command.ExecuteNonQuery();
+                }
             }
             catch (SqlException sqlEx)
             {
@@ -38,154 +35,239 @@ namespace Datos
             }
         }
 
+        // Método para obtener todos los detalles de la orden de trabajo
         public static List<DetalleOrdenTrabajo> GetAll()
         {
             var result = new List<DetalleOrdenTrabajo>();
             try
             {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
-                SqlConnection connection = new SqlConnection(conexion);
-                connection.Open();
-                SqlCommand command = new SqlCommand("GetAllDetalleOrdenTrabajo", connection);
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    DetalleOrdenTrabajo detalle = new DetalleOrdenTrabajo();
-                    detalle.id = Convert.ToInt32(reader["id"]);
-                    detalle.ordenDeTrabajo = Convert.ToInt32(reader["ordenDeTrabajo"]);
-                    detalle.insumo = Convert.ToInt32(reader["insumo"]);
-                    detalle.cantidad = Convert.ToDecimal(reader["cantidad"]);
-                    result.Add(detalle);
-                }
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            return result;
-        }
-
-        public static void InsertDetalleOrdenTrabajo(int ordenDeTrabajo, int insumo, decimal cantidad)
-        {
-            try
-            {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
-                SqlConnection connection = new SqlConnection(conexion);
-                SqlCommand command = new SqlCommand("InsertDetalleOrdenTrabajo", connection);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@ordenDeTrabajo", ordenDeTrabajo);
-                command.Parameters.AddWithValue("@insumo", insumo);
-                command.Parameters.AddWithValue("@cantidad", cantidad);
-                connection.Open();
-                command.ExecuteNonQuery();
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        public static void UpdateDetalleOrdenTrabajo(int id, int ordenDeTrabajo, int insumo, decimal cantidad)
-        {
-            try
-            {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
-                SqlConnection connection = new SqlConnection(conexion);
-                SqlCommand command = new SqlCommand("UpdateDetalleOrdenTrabajo", connection);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@id", id);
-                command.Parameters.AddWithValue("@ordenDeTrabajo", ordenDeTrabajo);
-                command.Parameters.AddWithValue("@insumo", insumo);
-                command.Parameters.AddWithValue("@cantidad", cantidad);
-                connection.Open();
-                command.ExecuteNonQuery();
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        public static DetalleOrdenTrabajo GetById(int id)
-        {
-            DetalleOrdenTrabajo result = new DetalleOrdenTrabajo();
-            try
-            {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
-                SqlConnection connection = new SqlConnection(conexion);
-                SqlCommand command = new SqlCommand("GetByIDDetalleOrdenTrabajo", connection);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@id", id);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    result = new DetalleOrdenTrabajo(
-                        Convert.ToInt32(reader["id"]),
-                        Convert.ToInt32(reader["ordenDeTrabajo"]),
-                        Convert.ToInt32(reader["insumo"]),
-                        Convert.ToDecimal(reader["cantidad"])
-                    );
-                }
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            return result;
-        }
-
-
-        public static List<int> GetAllInsumoIds()
-        {
-            var insumoIds = new List<int>();
-            try
-            {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
                 using (SqlConnection connection = new SqlConnection(conexion))
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand("SELECT idInsumo FROM insumo", connection);
+                    SqlCommand command = new SqlCommand("GetAllDetalleOrdenTrabajo", connection);
                     SqlDataReader reader = command.ExecuteReader();
+
                     while (reader.Read())
                     {
-                        insumoIds.Add(reader.GetInt32(0));
+                        var detalle = new DetalleOrdenTrabajo(
+                            Convert.ToInt32(reader["id"]),
+                            GetOrdenTrabajoById(Convert.ToInt32(reader["ordenDeTrabajo"])),
+                            GetInsumoById(Convert.ToInt32(reader["insumo"])),
+                            Convert.ToDecimal(reader["cantidad"])
+                        );
+                        result.Add(detalle);
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("error al cargar los insumos");
+                throw new Exception("Error al obtener los detalles de la orden de trabajo: " + ex.Message, ex);
             }
-            return insumoIds;
+            return result;
+        }
+
+        // Método para insertar un nuevo detalle de orden de trabajo
+        public static void InsertDetalleOrdenTrabajo(OrdenTrabajo ordenDeTrabajo, Insumo insumo, decimal cantidad)
+        {
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("InsertDetalleOrdenTrabajo", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ordenDeTrabajo", ordenDeTrabajo.id);
+                    command.Parameters.AddWithValue("@insumo", insumo.idInsumo);
+                    command.Parameters.AddWithValue("@cantidad", cantidad);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al insertar el detalle de la orden de trabajo: " + ex.Message, ex);
+            }
+        }
+
+        // Método para actualizar un detalle de orden de trabajo
+        public static void UpdateDetalleOrdenTrabajo(int id, OrdenTrabajo ordenDeTrabajo, Insumo insumo, decimal cantidad)
+        {
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("UpdateDetalleOrdenTrabajo", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@ordenDeTrabajo", ordenDeTrabajo.id);
+                    command.Parameters.AddWithValue("@insumo", insumo.idInsumo);
+                    command.Parameters.AddWithValue("@cantidad", cantidad);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al actualizar el detalle de la orden de trabajo: " + ex.Message, ex);
+            }
+        }
+
+        // Método para obtener un detalle de orden de trabajo por su id
+        public static DetalleOrdenTrabajo GetById(int id)
+        {
+            DetalleOrdenTrabajo result = null;
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("GetByIDDetalleOrdenTrabajo", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@id", id);
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result = new DetalleOrdenTrabajo(
+                            Convert.ToInt32(reader["id"]),
+                            GetOrdenTrabajoById(Convert.ToInt32(reader["ordenDeTrabajo"])),
+                            GetInsumoById(Convert.ToInt32(reader["insumo"])),
+                            Convert.ToDecimal(reader["cantidad"])
+                        );
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el detalle de la orden de trabajo: " + ex.Message, ex);
+            }
+            return result;
+        }
+
+        // Método auxiliar para obtener el objeto OrdenTrabajo por id
+        public static OrdenTrabajo GetOrdenTrabajoById(int id)
+        {
+            OrdenTrabajo ordenTrabajo = null;
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("SELECT * FROM OrdenDeTrabajo WHERE id = @id", connection);
+                    command.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        ordenTrabajo = new OrdenTrabajo
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            fechaLote = Convert.ToDateTime(reader["fechaLote"]),
+                            centroDeCosto = new CentroCosto
+                            {
+                                // Suponiendo que CentroCosto tiene una propiedad id
+                                idCentro = Convert.ToInt32(reader["centroDeCosto"])
+                            }
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener la orden de trabajo: " + ex.Message, ex);
+            }
+            return ordenTrabajo;
         }
 
         public static List<int> GetAllOrdenTrabajoIds()
         {
-            var ordenIds = new List<int>();
+            var result = new List<int>();
             try
             {
-                string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
                 using (SqlConnection connection = new SqlConnection(conexion))
                 {
-                    connection.Open();
                     SqlCommand command = new SqlCommand("SELECT id FROM OrdenDeTrabajo", connection);
+                    connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
+
                     while (reader.Read())
                     {
-                        ordenIds.Add(reader.GetInt32(0));
+                        result.Add(Convert.ToInt32(reader["id"]));
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("error las ordentrabajo");
+                throw new Exception("Error al obtener los ids de órdenes de trabajo: " + ex.Message, ex);
             }
-            return ordenIds;
+            return result;
         }
 
+        public static List<int> GetAllInsumoIds()
+        {
+            var result = new List<int>();
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("SELECT idInsumo FROM insumo", connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        result.Add(Convert.ToInt32(reader["idInsumo"]));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener los ids de insumos: " + ex.Message, ex);
+            }
+            return result;
+        }
+
+
+
+        // Método auxiliar para obtener el objeto Insumo por id
+        public static Insumo GetInsumoById(int id)
+        {
+            Insumo insumo = null;
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(conexion))
+                {
+                    SqlCommand command = new SqlCommand("SELECT * FROM insumo WHERE idInsumo = @id", connection);
+                    command.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        insumo = new Insumo
+                        {
+                            idInsumo = Convert.ToInt32(reader["idInsumo"]),
+                            detalle = reader["detalle"].ToString(),
+                            otros = reader["otros"] as string,
+                            cuenta = reader["cuenta"] as string
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el insumo: " + ex.Message, ex);
+            }
+            return insumo;
+        }
     }
 }
--- Presentation/FormDetalleOrdenTrabajo.cs
+++ Presentation/FormDetalleOrdenTrabajo.cs
@@ -1,4 +1,5 @@
 ﻿using Datos;
+using Modelos;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,16 +23,21 @@ namespace Presentation
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             if (CamposCompletos())
             {
                 try
                 {
-                    int ordenDeTrabajo = Convert.ToInt32(cb1.Text);
-                    int insumo = Convert.ToInt32(cb2.Text);
+                    int ordenDeTrabajoId = Convert.ToInt32(cb1.Text);
+                    int insumoId = Convert.ToInt32(cb2.Text);
                     decimal cantidad = Convert.ToDecimal(tv2.Text);
 
+                    // Obtener los objetos completos (OrdenTrabajo e Insumo) a partir de los IDs
+                    OrdenTrabajo ordenDeTrabajo = DDetalleOrdenTrabajo.GetOrdenTrabajoById(ordenDeTrabajoId);
+                    Insumo insumo = DDetalleOrdenTrabajo.GetInsumoById(insumoId);
+
+                    // Insertar el detalle de la orden de trabajo
                     DDetalleOrdenTrabajo.InsertDetalleOrdenTrabajo(ordenDeTrabajo, insumo, cantidad);
+
                     MessageBox.Show("Detalle de orden de trabajo insertado con éxito.");
                     VaciarCampos();
                 }
@@ -41,7 +47,6 @@ namespace Presentation
                 }
                 button6_Click(sender, e); // Es para refrescar los campos (llama al GetAll)
             }
-
             else
             {
                 MessageBox.Show("Faltan completar campos");
@@ -50,63 +55,65 @@ namespace Presentation
 
 
 
+
         private void button6_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var detalles = DDetalleOrdenTrabajo.GetAll();
 
+                dt1.Rows.Clear();
+
+                foreach (var detalle in detalles)
+                {
+                    // Mostrar los detalles con los objetos completos
+                    dt1.Rows.Add(detalle.id, detalle.ordenDeTrabajo.id, detalle.ordenDeTrabajo.fechaLote,
+                                 detalle.insumo.idInsumo, detalle.insumo.detalle, detalle.cantidad);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tv1.Text))
             {
                 try
                 {
-                    var detalles = DDetalleOrdenTrabajo.GetAll();
+
+                    int id = Convert.ToInt32(tv1.Text);
+
+
+                    var detalle = DDetalleOrdenTrabajo.GetById(id);
+
 
                     dt1.Rows.Clear();
 
 
-                    foreach (var detalle in detalles)
+                    if (detalle != null && detalle.id != 0) // Agregada la condicional esto por el BUG de los ceros
                     {
+
                         dt1.Rows.Add(detalle.id, detalle.ordenDeTrabajo, detalle.insumo, detalle.cantidad);
+
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("No existe ese ID");
                     }
                 }
+
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error");
+
+                    MessageBox.Show("Error grave");
+                    // Console.WriteLine(ex.ToString()); // Debugeando
                 }
-            }
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrEmpty(tv1.Text)) { 
-            try
-            {
-
-                int id = Convert.ToInt32(tv1.Text);
-
-
-                var detalle = DDetalleOrdenTrabajo.GetById(id);
-
-
-                dt1.Rows.Clear();
-
-
-                if (detalle != null && detalle.id != 0) // Agregada la condicional esto por el BUG de los ceros
-                {
-
-                    dt1.Rows.Add(detalle.id, detalle.ordenDeTrabajo, detalle.insumo, detalle.cantidad);
-
-                }
-                else
-                {
-
-                    MessageBox.Show("No existe ese ID");
-                }
-            }
-
-            catch (Exception ex)
-            {
-
-                MessageBox.Show("Error grave");
-                // Console.WriteLine(ex.ToString()); // Debugeando
-            }
 
             }
             else
@@ -125,23 +132,21 @@ namespace Presentation
         {
             if (CamposCompletos())
             {
-
-
-            {
                 try
                 {
-                    int id = Convert.ToInt32(tv1.Text); // _--> Esta ID modificamos:
-                    int ordenDeTrabajo = Convert.ToInt32(cb1.Text);
-                    int insumo = Convert.ToInt32(cb2.Text);
+                    int id = Convert.ToInt32(tv1.Text);
+                    int ordenDeTrabajoId = Convert.ToInt32(cb1.Text);
+                    int insumoId = Convert.ToInt32(cb2.Text);
                     decimal cantidad = Convert.ToDecimal(tv2.Text);
 
+                    // Obtener los objetos completos
+                    OrdenTrabajo ordenDeTrabajo = DDetalleOrdenTrabajo.GetOrdenTrabajoById(ordenDeTrabajoId);
+                    Insumo insumo = DDetalleOrdenTrabajo.GetInsumoById(insumoId);
 
+                    // Actualizar el detalle de la orden de trabajo
                     DDetalleOrdenTrabajo.UpdateDetalleOrdenTrabajo(id, ordenDeTrabajo, insumo, cantidad);
 
-
                     MessageBox.Show("Actualizado con exito");
-
-
                     button6_Click(sender, e); // Es para refrescar los campos (llama al GetAll)
                 }
                 catch (Exception)
@@ -149,8 +154,12 @@ namespace Presentation
                     MessageBox.Show("ERROR");
                 }
             }
+            else
+            {
+                MessageBox.Show("Por favor, complete todos los campos antes de actualizar.");
             }
         }
+
 
         private void cb1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -184,7 +193,7 @@ namespace Presentation
                 }
                 else if (control is ComboBox)
                 {
-                    ((ComboBox)control).SelectedIndex = -1; 
+                    ((ComboBox)control).SelectedIndex = -1;
                 }
                 else if (control is DataGridView)
                 {
@@ -197,7 +206,7 @@ namespace Presentation
         public void LoaderComboInsumo()
         {
             var insumoIds = DDetalleOrdenTrabajo.GetAllInsumoIds();
-            cb2.Items.Clear(); 
+            cb2.Items.Clear();
             foreach (var id in insumoIds)
             {
                 cb2.Items.Add(id);
@@ -214,17 +223,32 @@ namespace Presentation
             }
         }
 
-            private void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            int id = Convert.ToInt32(tv1.Text);
-            DDetalleOrdenTrabajo.DeleteDetalleOrdenTrabajo(id);
-            button6_Click(sender, e); // Es para refrescar los campos (llama al GetAll)
-            MessageBox.Show("Borrado con exito");
-            VaciarCampos();
+            if (string.IsNullOrWhiteSpace(tv1.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un ID para eliminar.");
+                return;
+            }
 
+            try
+            {
+                int id = Convert.ToInt32(tv1.Text);
+                DDetalleOrdenTrabajo.DeleteDetalleOrdenTrabajo(id);
+                button6_Click(sender, e); // Refresca los campos (llama a GetAll)
+                MessageBox.Show("Borrado con éxito");
+                VaciarCampos();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El ID debe ser un número válido.");
+            }
         }
+
+
     }
 }
 
-        
+
+
 
